25af7e9849c52a33b2d87e08ade77845966c017e
Dmitry <mr.dmitriy99@yandex.ru>
2018-07-11 21:38:08 +0300

    Initial commit

---
+++ ThirdTask/ThirdTask/Brackets.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTask {
+    class Brackets {
+
+        public static Boolean CheckBracketPlacement(String line) {
+            Int32 indexInStr = 0;          
+
+            return ToCheckInLine(ref indexInStr, line, ' ');
+        }
+
+        private static Boolean ToCheckInLine(ref Int32 index, String str, char bracket) {
+            Boolean flag = true;
+
+            switch (bracket) {
+                case '(': {
+                    while (index < str.Length && str[index] != ')' && flag) {
+                        if (str[index] == '(') {
+                            index++;
+                            flag = ToCheckInLine(ref index, str, '(');
+                        }
+                        else if (str[index] == '[') {
+                            index++;
+                            flag = ToCheckInLine(ref index, str, '[');
+                        }
+                        else if (str[index] == ']')
+                            flag = false;
+
+                        if (index == str.Length - 1)
+                            flag = false;
+
+                        index++;
+                    }
+                    break;
+                }
+                case '[': {
+                    while (index < str.Length && str[index] != ']' && flag) {
+                        if (str[index] == '(') {
+                            index++;
+                            flag = ToCheckInLine(ref index, str, '(');
+                        }
+                        else if (str[index] == '[') {
+                            index++;
+                            flag = ToCheckInLine(ref index, str, '[');
+                        }
+                        else if (str[index] == ')')
+                            flag = false;
+
+                        if (index == str.Length - 1)
+                            flag = false;
+
+                        index++;
+                    }
+                    break;
+                }
+                default: {
+                    while (index < str.Length && flag) {
+                        if (str[index] == '(') {
+                            index++;
+                            flag = ToCheckInLine(ref index, str, '(');
+                        }
+                        else if (str[index] == '[') {
+                            index++;
+                            flag = ToCheckInLine(ref index, str, '[');
+                        }
+                        else if (str[index] == ')' || str[index] == ']')
+                            flag = false;
+
+                        index++;
+                    }
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+}
---
+++ ThirdTask/ThirdTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdTask {
+    class Program {
+        static void Main(string[] args) {
+            String str, sel = "";
+
+            Console.WriteLine("\t### Insidious brackets ###");
+            do {
+                Console.WriteLine("Input a line with brackets to check their placement:");
+                str = Console.ReadLine();
+                Console.WriteLine();
+
+                if (Brackets.CheckBracketPlacement(str))
+                    Console.WriteLine("Brackets are located correctly!");
+                else
+                    Console.WriteLine("Brackets are located incorrectly!");
+
+                Console.WriteLine("Again? (y/n)");
+                sel = Console.ReadLine();
+                while(sel != "n" && sel != "N" && sel != "y" && sel != "Y") {
+                    Console.WriteLine("Wrong symbol!!!");
+                    Console.WriteLine("Again? (y/n)");
+                    sel = Console.ReadLine();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            } while (sel != "n" && sel != "N");
+        }
+    }
+}
